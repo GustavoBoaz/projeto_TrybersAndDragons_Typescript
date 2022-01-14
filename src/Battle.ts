7ebc3460069274efe72b2ecacb9a53aec801e5b8
Natanael Neto <nesn.gamedev@gmail.com>
2022-01-14 11:46:09 -0300

    Ajusta Battle.ts

--- src/Battle.ts
+++ src/Battle.ts
@@ -1,13 +1,20 @@
 import Fighter from './Fighter';
 
 class Battle {
-  private $fighters: Fighter[];
-
-  constructor(fighters: Fighter[]) {
-    this.$fighters = fighters;
+  static enterDungeon(invader: Fighter, owner: Fighter): Fighter {
+    while (true) {
+      invader.attack(owner);
+      if (owner.lifePoints > 0) {
+        owner.attack(invader);
+        if (invader.lifePoints <= 0) {
+          return owner;
+        }
+      }
+      else {
+        return invader;
+      }
+    }
   }
-  // TODO: implementar aqui em baixo
-  // static Fight(): void { }
 }
 
 export default Battle;
