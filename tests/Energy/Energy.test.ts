6310e2c21dca61c9d7781f6d1ceb60516195eca8
Andre Vicente <isquicha@gmail.com>
2022-01-14 18:52:44 -0300

    Add novos testes da energy

--- tests/Energy/Energy.test.ts
+++ tests/Energy/Energy.test.ts
@@ -1,9 +1,18 @@
 import Energy, { EnergyType } from '../../src/Energy';
+import cp from 'child_process';
+import path from 'path';
 
 describe('Energy', () => {
-  describe('Tipo EnergyType', () => {
+  describe.only('Tipo EnergyType', () => {
     it('É possível criar uma variável com o tipo EnergyType e atribuir a ela o valor "mana"', () => {
-      let x: EnergyType = 'mana';
+      expect(() => {
+        cp.exec(`npx tsc ${path.resolve(path.join(__dirname, 'Energy.mana.ts'))}`);
+      }).not.toThrow();
+    });
+    it('Não possível criar uma variável com o tipo EnergyType e atribuir a ela o valor "mani"', () => {
+      expect(() => {
+        cp.exec(`npx tsc ${path.resolve(path.join(__dirname, 'Energy.mani.ts'))}`);
+      }).toThrow();
     });
     it('É possível criar uma variável com o tipo EnergyType e atribuir a ela o valor "stamina"', () => {
       let x: EnergyType = 'stamina';
