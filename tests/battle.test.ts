d8593855f79e1ecf599aa00b2b2751d8f65fd58a
Natanael Neto <nesn.gamedev@gmail.com>
2022-01-19 10:46:45 -0300

    Inicia testes do Battle

--- tests/battle.test.ts
+++ tests/battle.test.ts
@@ -1,3 +1,10 @@
-it('', () => {
-
+describe('Battle', () => {
+  describe('Classe Battle', () => {
+    it('A classe Battle existe', () => {
+      expect('Battle.exists').toCompile();
+    });
+    it('A função fight pode ser acessada', () => {
+      expect('Battle.fightAcess').toCompile();
+    });
+  });
 });
---
+++ tests/sources/Battle.exists.ts
@@ -0,0 +1,7 @@
+import Battle from "../../src/Battle";
+import Character from "../../src/Character";
+import Monster from "../../src/Monster";
+
+const myPLayer = new Character('');
+const myMonster = new Monster();
+const battle = new Battle(myPLayer, [myMonster]);
