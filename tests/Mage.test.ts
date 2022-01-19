4346a1ab4ea576d50759e36bcd2c258f17f7b397
Andre Vicente <isquicha@gmail.com>
2022-01-19 01:00:45 -0300

    Update teste do Mage

--- tests/Mage.test.ts
+++ tests/Mage.test.ts
@@ -4,7 +4,7 @@ describe('3 - Crie a classe Mage que herda de Archetype', function () {
     expect('Mage.exists.ts').toCompile();
   });
   it('O parâmetro name da classe Mage pode ser lido', async () => {
-    expect('x.ts').toCompile();
+    expect('Mage.name.ts').toCompile();
     const jsString = fs.readFileSync('./tests/sources/Mage.name.js') + ' ; result();';
     const result = eval(jsString.replace('../../src', '../src'));
     expect(result).toBe('Alex');
