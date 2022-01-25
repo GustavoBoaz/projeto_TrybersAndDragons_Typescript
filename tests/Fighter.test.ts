d3cdd4058efc8f910a004fbb1878dba45f42bc66
Andre Vicente <isquicha@gmail.com>
2022-01-25 10:38:38 -0300

    Adiciona esqueleto dos testes de Fighter

---
+++ tests/Fighter.test.ts
@@ -0,0 +1,10 @@
+describe('Fighter', () => {
+  describe('6 - Interface Fighter', () => {
+    it('A interface Fighter existe', () => {
+      expect('Fighter.exists').toCompile();
+    });
+    it('A interface fighter possui o atributo', () => {
+      expect('Fighter.').toCompile();
+    });
+  });
+});
