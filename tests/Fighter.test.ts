e5e12cfcd0cef810e87b6b8df5f22bfd19fda781
Andre Vicente <isquicha@gmail.com>
2022-01-25 23:16:06 -0300

    Add Fighter tests

--- tests/Fighter.test.ts
+++ tests/Fighter.test.ts
@@ -3,8 +3,24 @@ describe('Fighter', () => {
     it('A interface Fighter existe', () => {
       expect('Fighter.exists').toCompile();
     });
-    it('A interface fighter possui o atributo', () => {
-      expect('Fighter.').toCompile();
+    it('A interface Fighter pode ser implementada corretamente', () => {
+      expect('Fighter.implemented').toCompile();
+    });
+    it('A interface Fighter possui o atributo lifePoints', () => {
+      expect('Fighter.lifePoints').notToCompile();
+      // TODO: Testar erros com tipos diferentes de number
+    });
+    it('A interface Fighter possui o atributo strength', () => {
+      expect('Fighter.strength').notToCompile();
+    });
+    it('A interface Fighter possui o atributo defense', () => {
+      expect('Fighter.defense').notToCompile();
+    });
+    it('A interface Fighter possui o método attack, que recebe um enemy do tipo Fighter', () => {
+      expect('Fighter.attack').notToCompile();
+    });
+    it('A interface Fighter possui o método receiveDamage, que recebe um amount do tipo number', () => {
+      expect('Fighter.receiveDamage').notToCompile();
     });
   });
 });
---
+++ tests/sources/Fighter.receiveDamage.ts
@@ -0,0 +1,12 @@
+import Fighter from '../../src/Fighter';
+
+class f implements Fighter {
+  constructor(
+    public lifePoints: 100,
+    public strength: 10,
+    public defense: 10,
+  ) { }
+
+  attack(enemy: Fighter) {
+  }
+};
