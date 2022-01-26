dded3d43d0d815049dcab6432a8952b25572be01
Andre Vicente <isquicha@gmail.com>
2022-01-26 05:16:35 -0300

    Add testes Character

--- tests/07 - Character.test.ts
+++ tests/07 - Character.test.ts
@@ -1,4 +1,14 @@
 describe('07 - Classe Character', () => {
-  it('Should pass', () => {
+  it('A classe Character existe', () => {
+    expect('Character.exists').toCompile();
+  });
+  it('A classe Character implementa a interface Fighter', () => {
+    expect('Character.implements').toCompile();
+  });
+  it('Um Character possui uma Race', () => {
+    expect('Character.race').toCompile();
+  });
+  it('Um Character possui um Archetype', () => {
+    expect('Character.archetype').toCompile();
   });
 });
