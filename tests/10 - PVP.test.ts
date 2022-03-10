af91fefbb7b19bfd05c5318f48841e811c1b7545
Vinicius-Vasconcelos <viniciussouzav@gmail.com>
2022-03-10 10:45:26 -0300

    Update tests/10 - PVP.test.ts
    Co-authored-by: Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>

--- tests/10 - PVP.test.ts
+++ tests/10 - PVP.test.ts
@@ -2,7 +2,7 @@ describe('10 - Classe PVP', () => {
   it('A classe PVP existe e se pode ser criada uma nova instância, passando dois Characters lutadores', () => {
     expect('PVP.exists').toCompile();
   });
-  it('A classe PVP pode ser utilizada onde a classe Battle é esperadae um personagem que chamou várias vezes o levelUp e possue melhores atributos tem maiores chances de vencer', () => {
+  it('A classe PVP pode ser utilizada onde a classe Battle é esperada, um personagem que chamou várias vezes o levelUp e possui melhores atributos tem maiores chances de vencer', () => {
     expect('PVP.LSP').toCompileAndBeEqualTo([1, -1]);
   });
   it('A classe PVP pode receber tanto dois Characters quanto duas instâncias de uma implementação diferente de Fighter', () => {
